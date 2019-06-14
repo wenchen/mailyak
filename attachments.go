5b3fb2078c0a67f00ec1bf323c547f0b7e0a9b51
Yen Wen Chen <wenchen.x@gmail.com>
2019-06-14 17:03:04 -0400

    Fixed missing Content-Id information of inline-attachments

--- attachments.go
+++ attachments.go
@@ -145,10 +145,13 @@ func getMIMEHeader(a attachment, ctype string) textproto.MIMEHeader {
 
 	if a.inline {
 		disp = fmt.Sprintf("inline;\n\tfilename=%q", a.filename)
+		cid := fmt.Sprintf("<%s>", a.filename)
+
 		header = textproto.MIMEHeader{
 			"Content-Type":              {ctype},
 			"Content-Disposition":       {disp},
 			"Content-Transfer-Encoding": {"base64"},
+			"Content-ID":                {cid},
 		}
 	} else {
 		disp = fmt.Sprintf("attachment;\n\tfilename=%q", a.filename)
